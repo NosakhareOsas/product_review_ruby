23d9bd71869cfb6c1b63a395766c13cb51f3f4da
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-09-11 14:15:48 +0100

    print_all_reviews added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,15 @@
 class Product < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
+
+    def leave_review(user, star_rating, comment)
+        Review.create(star_rating: star_rating, comment: comment, user: user, product: self)
+    end
+
+    def print_all_reviews
+        self.reviews.each do |review|
+            puts "Review for #{review.product.name} by #{review.user.name}: #{review.star_rating}. #{review.comment}"
+        end
+    end 
+
 end
